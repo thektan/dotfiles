05742fe65cf4da6f9eb64182d9e010151c297c05
Kevin Tan <kevin.tan@liferay.com>
2017-03-08 10:56:02 -0800

    Add brew and brew-cask packages

---
+++ brew-cask.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# System
+brew cask install 1password
+brew cask install dropbox
+brew cask install google-drive
+brew cask install private-internet-access
+
+brew cask install google-chrome
+brew cask install firefox
+
+# Productivity
+
+# Tools
+brew cask install helium
+brew cask install kap
+brew cask install transmission
+
+# Social
+brew cask install caprine # (https://github.com/sindresorhus/caprine)
+brew cask install discord
+brew cask install skype
+
+# Dev Apps
+brew cask install atom
+brew cask install iterm2
+brew cask install github-desktop
+brew cask install sublime-text
+brew cask install sequel-pro
+
+# References
+brew cask install dash
+
+# Design Apps
+brew cask install sketch
+
+# Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package qlvideo
+
+# Other apps that aren't in cask
+# https://www.wunderlist.com/download/
+# https://getpocket.com/
+# http://matthewpalmer.net/rocket/
+# https://getcaption.co/
---
+++ brew.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Install command line tools with Homebrew.
+
+brew install git
+brew install diff-so-fancy
+brew install the_silver_searcher
+brew install tree
+
+# Extra tools
+
+brew install shpotify
+brew install wifi-password
