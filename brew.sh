a1fe02e793bcdecdcac7cbe6c21d02fb369ed1e5
Kevin Tan <kevin.tan@liferay.com>
2017-03-13 15:21:16 -0700

    Update brew.sh - install node, zsh

--- brew.sh
+++ brew.sh
@@ -2,10 +2,18 @@
 
 # Install command line tools with Homebrew.
 
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
 brew install git
 brew install diff-so-fancy
+brew install node
 brew install the_silver_searcher
 brew install tree
+brew install zsh
 
 # Extra tools
 
