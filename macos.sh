7116c5526f7df71f2b73ace1b336eadc6cc434ed
Kevin Tan <kevin.tan@liferay.com>
2017-03-13 21:47:15 -0700

    Add macos.sh

---
+++ macos.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Finder: Show all hidden files by default
+defaults write -g AppleShowAllFiles -bool true
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
