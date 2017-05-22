d4ac81c27d0221d04375042c07ae9e26f354f4cf
Kevin Tan <kevin.tan@liferay.com>
2017-05-22 10:24:07 -0700

    Add setup symlinks for sublime perferences

--- setup.sh
+++ setup.sh
@@ -5,3 +5,5 @@ export DOTFILES_HOME=${HOME}/.dotfiles
 # Create symlinks
 ln -s ${DOTFILES_HOME}/.zshrc ~/.zshrc
 ln -s ${DOTFILES_HOME}/.editorconfig ~/.editorconfig
+ln -s ${DOTFILES_HOME}/sublime/Preferences.sublime-settings  ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+ln -s ${DOTFILES_HOME}/sublime/Package\ Control.sublime-settings  ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
