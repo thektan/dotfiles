cf4f51ac0cd92e8e46bf92517d09d06dd3788c24
Kevin Tan <kevin.tan@liferay.com>
2017-08-29 10:55:05 -0700

    Add cask install ship
    Application for managing issues. Makes it easy to jump between project boards and see a full listing of issues.
    https://www.realartists.com/index.html

--- brew-cask.sh
+++ brew-cask.sh
@@ -24,6 +24,7 @@ brew cask install spotify
 brew cask install atom
 brew cask install iterm2
 brew cask install github-desktop
+brew cask install ship # https://www.realartists.com/index.html
 brew cask install sublime-text
 brew cask install sequel-pro
 brew cask install visual-studio-code
