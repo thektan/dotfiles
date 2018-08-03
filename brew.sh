474816210d3565a42d664e5cde0984907e2dfdee
Kevin Tan <kevin.tan@liferay.com>
2018-08-03 14:37:42 -0700

    Add some docs on what these brew packages are

--- brew.sh
+++ brew.sh
@@ -10,9 +10,13 @@ brew upgrade
 
 brew install git
 brew install coreutils              # https://en.wikipedia.org/wiki/GNU_Core_Utilities
+                                    # Provides `gls` instead of `ls` to list directories first.
+                                    # More about it in this link: https://superuser.com/questions/109537/how-to-sort-first-directories-then-files-etc-when-using-ls-in-unix
 brew install diff-so-fancy          # https://github.com/so-fancy/diff-so-fancy/
 brew install fd                     # https://github.com/sharkdp/fd
+                                    # Faster alternative to `find`
 brew install m-cli                  # https://github.com/rgcr/m-cli
+                                    # Provides utility macOS commands.
 brew install node
 brew install ripgrep                # https://github.com/BurntSushi/ripgrep
 brew install ruby
@@ -23,7 +27,9 @@ brew install zsh
 
 # Extra tools
 brew install shpotify               # https://github.com/hnarayanan/shpotify
+                                    # CLI for Spotify
 brew install wifi-password          # https://github.com/rauchg/wifi-password
+                                    # Gets the wifi password
 
 # Misc Development
 # brew install md5sha1sum           # https://raamdev.com/2008/howto-install-md5sum-sha1sum-on-mac-os-x/
