2188cc0054f55c17699aaa38bccc790306923461
Kevin Tan <kevin.tan@liferay.com>
2019-10-01 15:58:19 -0700

    fzf: Update fzf to use fd and respect .gitignore
    https://github.com/junegunn/fzf#respecting-gitignore

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,14 +1,16 @@
 # Setup fzf
-# ---------
 if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
   export PATH="$PATH:/usr/local/opt/fzf/bin"
 fi
 
 # Auto-completion
-# ---------------
 [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
-# ------------
 source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 
+# Setting fd as the default source for fzf
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+
+# Apply the command to CTRL-T as well
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
