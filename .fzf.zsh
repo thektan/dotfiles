ed2984d20637c7115f855523518469422c4eea5b
Kevin Tan <kevin.tan@liferay.com>
2019-10-01 17:20:03 -0700

    fzf: Use bat as preview and open in vscode

--- .fzf.zsh
+++ .fzf.zsh
@@ -12,5 +12,11 @@ source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 # Setting fd as the default source for fzf
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 
+# Full height mode --no-height
+# Use ctrl+o to open selected file(s) in VS Code
+export FZF_DEFAULT_OPTS="--no-height --bind='ctrl-o:execute(code {})+abort'"
+
 # Apply the command to CTRL-T as well
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+export FZF_CTRL_T_OPTS="--preview-window 'right:60%' --preview 'bat --color=always --style=header,grid --line-range :300 {}'"
